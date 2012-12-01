3b45a801c9fe5ab43f817fea71d2a6bde4088189
rucha <rucha@ubuntu.(none)>
2012-12-01 03:20:51 -0500

    f

---
+++ Makefile
@@ -0,0 +1,55 @@
+include ./Make.defines
+#include /home/users/cse533/Stevens/unpv13e/Make.defines
+# This is a sample Makefile which compiles source files named:
+# - tcpechotimeserv.c
+# - tcpechotimecliv.c
+# - time_cli.c
+# - echo_cli.c
+# and creating executables: "server", "client", "time_cli"
+# and "echo_cli", respectively.
+#
+# It uses various standard libraries, and the copy of Stevens'
+# library "libunp.a" in ~cse533/Stevens/unpv13e_solaris2.10 .
+#
+# It also picks up the thread-safe version of "readline.c"
+# from Stevens' directory "threads" and uses it when building
+# the executable "server".
+#
+# It is set up, for illustrative purposes, to enable you to use
+# the Stevens code in the ~cse533/Stevens/unpv13e_solaris2.10/lib
+# subdirectory (where, for example, the file "unp.h" is located)
+# without your needing to maintain your own, local copies of that
+# code, and without your needing to include such code in the
+# submissions of your assignments.
+#
+# Modify it as needed, and include it with your submission.
+
+CC = gcc
+
+LIBS = /home/users/cse533/Stevens/unpv13e/libunp.a -lpthread\
+
+FLAGS = -w -g -O2
+
+CFLAGS = ${FLAGS} -I/home/users/cse533/Stevens/unpv13e/lib
+
+
+all: arp  \
+#	get_hw_addrs.o prhwaddrs.o ${CC} -o prhwaddrs prhwaddrs.o get_hw_addrs.o ${LIBS}
+
+#app_functions.o: app_functions.c
+#	${CC} ${CFLAGS} -c app_functions.c
+
+get_hw_addrs.o: get_hw_addrs.c
+	${CC} ${FLAGS} -c get_hw_addrs.c
+
+arp: arp.o get_hw_addrs.o
+	${CC} ${FLAGS} -o arp arp.o get_hw_addrs.o ${LIBS}
+arp.o: arp.c
+	${CC} ${CFLAGS} -c arp.c
+
+
+
+	
+clean:
+	rm arp arp.o get_hw_addrs.o
+
---
+++ arp.c
@@ -0,0 +1,339 @@
+
+#include <sys/socket.h>
+//#include <netpacket/packet.h>
+//#include <net/ethernet.h> /* the L2 protocols */
+#include "hw_addrs.h"
+#include "unp.h"
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+#define USID_PROTO 0x4481
+#define UNIXDG_PATH "testpath"
+#define ETH_FRAME_LEN 1514
+
+struct arp_frame
+{
+	uint16_t id;	
+	uint16_t hard_type;
+	uint16_t proto_type;
+	uint8_t hard_size;
+	uint8_t proto_size;
+	uint16_t op;
+	char sender_ethernet_address[6];
+	char sender_ip_address[4];
+	char target_ethernet_address[6];
+	char target_ip_address[4];
+};
+
+struct IP_hw_address_mpg
+{
+	char ip_address[100];
+	char hw_address[6];
+	int sll_ifindex;
+	unsigned short sll_hatype;
+	int unix_domain_confd;
+};
+
+
+void sendARPframe( int s , struct arp_frame * populated_arp_frame , char * source_hw_mac_address, char * destination_hw_mac_address , int if_index )
+{
+	
+	int j,i;
+	/*target address*/
+	struct sockaddr_ll socket_address;
+
+	/*buffer for ethernet frame*/
+	void* buffer = (void*)malloc(ETH_FRAME_LEN);
+	 
+	/*pointer to ethenet header*/
+	unsigned char* etherhead = buffer;
+		
+	/*pointer to userdata in ethernet frame*/
+	unsigned char* data = buffer + 14;
+		
+	/*another pointer to ethernet header*/
+	struct ethhdr *eh = (struct ethhdr *)etherhead;
+	 
+	int send_result = 0,k=0;
+
+	/*our MAC address*/
+	unsigned char src_mac[6], dest_mac[6]; 
+
+	char * src_mac1=source_hw_mac_address; 
+	char * dest_mac1=destination_hw_mac_address; 
+
+
+//	unsigned char src_mac[6] = {0x00, 0x0c, 0x29, 0x11, 0x58, 0xa2};
+	
+	/*Broadcast MAC address*/
+	//unsigned char dest_mac[6] = {0x00, 0x0c, 0x29, 0x24, 0x8f, 0x70};
+	//unsigned char dest_mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+	
+	i = IF_HADDR;
+	printf("\n\nSending ARP Frame \n Destination H/W Address :\n");
+	do 
+	{	
+		dest_mac[k] = *destination_hw_mac_address++ & 0xff;
+		k++;
+		printf("%.2x%s", *dest_mac1++ & 0xff, (i == 1) ? " " : ":");
+	} while (--i > 0);
+	printf("\n");
+	
+
+	//printf("\tsending frame on socket: %d\n",s );
+	//printf("\tpopulated_odr_frame: %d bytes.\n",sizeof(*populated_odr_frame));
+	/*prepare sockaddr_ll*/
+	
+	printf(" Source H/W Address :\n");
+	i = IF_HADDR;
+	k=0;
+	do 
+	{	
+		src_mac[k] = *source_hw_mac_address++ & 0xff;
+		k++;
+		printf("%.2x%s", *src_mac1++ & 0xff, (i == 1) ? " " : ":");
+	} while (--i > 0);
+
+	printf("\n");
+	/*RAW communication*/
+	socket_address.sll_family   = PF_PACKET;	
+	/*we don't use a protocoll above ethernet layer
+	  ->just use anything here*/
+	socket_address.sll_protocol = htons(ETH_P_IP);	
+
+	/*ARP hardware identifier is ethernet*/
+	socket_address.sll_hatype   = ARPHRD_ETHER;
+		
+	/*target is another host*/
+	socket_address.sll_pkttype  = PACKET_OTHERHOST;
+
+	/*index of the network device
+	see full code later how to retrieve it*/
+	socket_address.sll_ifindex  = if_index;
+
+	/*address length*/
+	socket_address.sll_halen    = ETH_ALEN;		
+
+	//printf("Before socket_address..\n");	
+	/*MAC - begin*/
+	socket_address.sll_addr[0]  = dest_mac[0];		
+	socket_address.sll_addr[1]  = dest_mac[1];		
+	socket_address.sll_addr[2]  = dest_mac[2];
+	socket_address.sll_addr[3]  = dest_mac[3];
+	socket_address.sll_addr[4]  = dest_mac[4];
+	socket_address.sll_addr[5]  = dest_mac[5];
+	/*MAC - end*/
+	socket_address.sll_addr[6]  = 0x00;/*not used*/
+	socket_address.sll_addr[7]  = 0x00;/*not used*/
+
+	/*set the frame header*/
+	memcpy((void*)buffer, (void*)dest_mac, ETH_ALEN);
+	memcpy((void*)(buffer+ETH_ALEN), (void*)src_mac, ETH_ALEN);
+	eh->h_proto = htons(USID_PROTO);
+	/*fill the frame with some data*/
+	memcpy((void*)data,(void*)populated_arp_frame, sizeof( struct arp_frame ));
+/*
+	for (j = 0; j < 1500; j++) {
+		data[j] = (unsigned char)((int) (255.0*rand()/(RAND_MAX+1.0)));
+	}
+*/
+	//printf("Just before send.. \n");
+	/*send the packet*/
+	send_result = sendto(s, buffer, ETH_FRAME_LEN, 0, 
+		      (struct sockaddr*)&socket_address, sizeof(socket_address));
+	if (send_result == -1){ perror("sendto"); }
+	//printf("Done sending..WOO\n");
+}
+
+struct arp_frame * processRecievedPacket(char * str_from_sock)
+{
+
+	struct arp_frame * recieved_arp_frame;
+	int j;
+	struct sockaddr_ll socket_address; 	/*target address*/
+	void* buffer = (void*)malloc(ETH_FRAME_LEN); 	/*buffer for ethernet frame*/
+	unsigned char* etherhead = buffer; 	/*pointer to ethenet header*/
+	void * data = buffer + 14;
+	
+	printf("\nBeginning processing of recieved packet..\n");
+		
+	/*pointer to userdata in ethernet frame*/
+	
+	memcpy((void*)buffer, (void*)str_from_sock, ETH_FRAME_LEN ); 
+		
+	recieved_arp_frame = (struct arp_frame *)data;
+	
+	printf("Converting to Host Byte Order..\n");
+	recieved_arp_frame = convertToHostByteOrder( recieved_arp_frame );
+
+	printf("Done processing of recieved packet..\n");
+	return recieved_arp_frame;
+
+}
+
+struct arp_frame * convertToHostByteOrder(struct arp_frame * recieved_arp_frame)
+{
+
+	recieved_arp_frame->id=htons(recieved_arp_frame->id);	
+	recieved_arp_frame->hard_type=htons(recieved_arp_frame->hard_type);
+	recieved_arp_frame->proto_type=htons(recieved_arp_frame->proto_type);
+	recieved_arp_frame->hard_size=htons(recieved_arp_frame->hard_size);
+	recieved_arp_frame->proto_size=htons(recieved_arp_frame->proto_size);
+	recieved_arp_frame->op=htons(recieved_arp_frame->op);
+	return recieved_arp_frame;
+}
+
+int main(int argc, char const *argv[])
+{
+
+		struct hwa_info	*hwa, *hwahead;
+		char * ifname_split;
+		int i, prflag, k, pf_socket, unix_domain_socket, arplen, ihw, khw, maxfdp;
+		char   *ptr,*ptr1, *hw_address1, sender_ethernet_address[6], buffer[ETH_FRAME_LEN+1];
+		struct sockaddr	*sa;
+		struct sockaddr_un unixaddr;
+		struct sockaddr_ll arpaddr;
+		char * dest_mac1; 
+		struct IP_hw_address_mpg IP_hw_address_mpg_collection, cache;
+		struct arp_frame *recvd_packet;
+		int nready, client[FD_SETSIZE];
+		ssize_t	n;
+		fd_set	rset, allset;
+
+		for (hwahead = hwa = Get_hw_addrs(); hwa != NULL; hwa = hwa->hwa_next) 
+		{
+			ifname_split = strtok(hwa->if_name, ":"); //get pointer to first token found and store in 0
+		
+			if( strcmp(ifname_split, "eth0")==0 )
+			{	
+				
+				if ( (sa = hwa->ip_addr) != NULL)
+				{
+					strcpy(IP_hw_address_mpg_collection.ip_address, Sock_ntop_host(sa, sizeof(*sa)));
+					printf("IP: %s\n", IP_hw_address_mpg_collection.ip_address);
+				}	
+				prflag = 0;
+				i = 0;
+				do 
+				{
+
+					if (hwa->if_haddr[i] != '\0') 
+					{
+						prflag = 1;
+						break;
+					}
+				} while (++i < IF_HADDR);
+			}
+			k=0;
+			if (prflag) 
+			{
+				printf("HW addr = ");
+				ptr = hwa->if_haddr;
+				hw_address1=ptr;
+				i = IF_HADDR;
+				do {
+					IP_hw_address_mpg_collection.hw_address[k] = *ptr++ & 0xff;
+					k++;
+					printf("%.2x%s", *hw_address1++ & 0xff, (i == 1) ? " " : ":");
+				} while (--i > 0);
+				printf("\n");
+			}
+		}
+
+	if((pf_socket = socket(PF_PACKET, SOCK_RAW, htons(USID_PROTO)))==-1)
+ 	{
+ 		printf("Error in creation of socket for PF_PACKET\n");
+ 		perror("socket");
+ 		return 0;
+ 	}
+ 
+ 	if((unix_domain_socket = socket(AF_LOCAL, SOCK_STREAM, 0))==-1)
+ 	{
+ 		printf("Error in creation of Unix Domain socket\n");
+		perror("socket");
+		return;
+		
+	}
+
+	unlink(UNIXDG_PATH);
+	bzero(&unixaddr, sizeof(unixaddr));
+	unixaddr.sun_family = AF_LOCAL;
+	strcpy(unixaddr.sun_path, UNIXDG_PATH);
+	
+    if(bind(unix_domain_socket, (SA *) &unixaddr, SUN_LEN(&unixaddr))<0)
+    {       fprintf(stderr,"bind() failed. errorno =  %d\n",errno); 
+            exit(1);
+    }
+	
+    if(listen(unix_domain_socket, LISTENQ)<0)
+    {       fprintf(stderr,"listen() failed. errorno = %d\n",errno);       
+            exit(1);
+    }
+
+    for (i = 0; i < FD_SETSIZE; i++)
+		client[i] = -1;			/* -1 indicates available entry */
+	
+	FD_ZERO(&allset);
+	FD_SET(unix_domain_socket, &allset);
+	FD_SET(pf_socket, &allset);
+	maxfdp=max(unix_domain_socket,pf_socket)+1;
+	/* end fig01 */
+
+	/* include fig02 */
+	for ( ; ; ) 
+	{
+		
+		rset = allset;		/* structure assignment */
+		if((nready = select(maxfdp, &rset, NULL, NULL, NULL))<0)
+		{	
+			if(errno==EINTR)	
+				continue;
+			else
+				err_sys("select error");	
+		}		
+		if (FD_ISSET(unix_domain_socket, &rset)) 
+		{	
+
+		}
+//---------------------------------------------------------------------------------------------------------
+
+        if (FD_ISSET(pf_socket, &rset)) 
+        {        /* new client connection */
+ 
+ 			printf("Receiving packet ...\n");
+        	arplen=sizeof(arpaddr);
+	        if((n=recvfrom(pf_socket,buffer, ETH_FRAME_LEN+1, 0, &arpaddr, &arplen)>0))
+	        {
+
+				ihw = IF_HADDR;
+	        	khw=0;
+
+	        		printf("Received packet from hw address:\n");
+				do 
+				{	
+					sender_ethernet_address[khw] = arpaddr.sll_addr[khw] & 0xff;
+					
+					printf("%.2x%s", sender_ethernet_address[khw] & 0xff, (ihw == 1) ? " " : ":");
+					khw++;
+				} while (--ihw > 0);
+				printf("\n at interface %d...\n",arpaddr.sll_ifindex);
+
+	        	
+
+	        	if (n == -1)
+	        	{ 
+	        		printf("Error in recieving data from ODR..\n"); 
+	        		exit(0);
+	        	}
+	        	else
+	        	{ 
+	        		printf("Recieved Packet successfully\n" ); 
+	        	}
+
+	            recvd_packet = (struct arp_frame *)processRecievedPacket(buffer);
+	        }
+
+        }
+
+    }
+	return 0;
+}

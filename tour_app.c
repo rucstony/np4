c2715d402b6a5b021f5738e4e1a9548905f357f5
rucha <rucha@ubuntu.(none)>
2012-12-04 01:10:14 -0500

    df

--- tour_app.c
+++ tour_app.c
@@ -1,14 +1,130 @@
 #include  "unp.h"
 #include <netinet/ip.h>
-
+#include    "ping.h"
+#define IDENTIFIER 72217
 #define MAX_PAYLOAD_SIZE 1500
 #define IPPROTO_USID    537    
 #define HOSTNAME_LEN 255
+
+//struct proto    proto_v4 = { proc_v4, send_v4, NULL, NULL, NULL, 0, IPPROTO_ICMP };
+
+struct proto {
+  struct sockaddr  *sasend; /* sockaddr{} for send, from getaddrinfo */
+  struct sockaddr  *sarecv; /* sockaddr{} for receiving */
+  socklen_t     salen;      /* length of sockaddr{}s */
+  int           icmpproto;  /* IPPROTO_xxx value for ICMP */
+} *pr;
+
+
+void readloop(int sockfd)
+{
+    int             size;
+    char            recvbuf[BUFSIZE];
+    char            controlbuf[BUFSIZE];
+    struct msghdr   msg;
+    struct iovec    iov;
+    ssize_t         n;
+    struct timeval  tval;
+
+
+    size = 60 * 1024;       /* OK if setsockopt fails */
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
+
+    sig_alrm(SIGALRM);      /* send first packet */
+
+    iov.iov_base = recvbuf;
+    iov.iov_len = sizeof(recvbuf);
+    msg.msg_name = pr->sarecv;
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    msg.msg_control = controlbuf;
+    for ( ; ; ) {
+        msg.msg_namelen = pr->salen;
+        msg.msg_controllen = sizeof(controlbuf);
+        n = recvmsg(sockfd, &msg, 0);
+        if (n < 0) {
+            if (errno == EINTR)
+                continue;
+            else
+                err_sys("recvmsg error");
+        }
+
+        Gettimeofday(&tval, NULL);
+        proc_v4(recvbuf, n, &msg, &tval);
+    }
+}
+
+void proc_v4(char *ptr, ssize_t len, struct msghdr *msg, struct timeval *tvrecv)
+{
+    int             hlen1, icmplen;
+    double          rtt;
+    struct ip       *ip;
+    struct icmp     *icmp;
+    struct timeval  *tvsend;
+
+    ip = (struct ip *) ptr;     /* start of IP header */
+    hlen1 = ip->ip_hl << 2;     /* length of IP header */
+    if (ip->ip_p != IPPROTO_ICMP)
+        return;             /* not ICMP */
+
+    icmp = (struct icmp *) (ptr + hlen1);   /* start of ICMP header */
+    if ( (icmplen = len - hlen1) < 8)
+        return;             /* malformed packet */
+
+    if (icmp->icmp_type == ICMP_ECHOREPLY) {
+        if (icmp->icmp_id != pid)
+            return;         /* not a response to our ECHO_REQUEST */
+        if (icmplen < 16)
+            return;         /* not enough data to use */
+
+        tvsend = (struct timeval *) icmp->icmp_data;
+        tv_sub(tvrecv, tvsend);
+        rtt = tvrecv->tv_sec * 1000.0 + tvrecv->tv_usec / 1000.0;
+
+        printf("%d bytes from %s: seq=%u, ttl=%d, rtt=%.3f ms\n",
+                icmplen, Sock_ntop_host(pr->sarecv, pr->salen),
+                icmp->icmp_seq, ip->ip_ttl, rtt);
+
+    } else if (verbose) {
+        printf("  %d bytes from %s: type = %d, code = %d\n",
+                icmplen, Sock_ntop_host(pr->sarecv, pr->salen),
+                icmp->icmp_type, icmp->icmp_code);
+    }
+}
+
+void send_v4(void)
+{
+    int         len;
+    struct icmp *icmp;
+
+    icmp = (struct icmp *) sendbuf;
+    icmp->icmp_type = ICMP_ECHO;
+    icmp->icmp_code = 0;
+    icmp->icmp_id = pid;
+    icmp->icmp_seq = nsent++;
+    memset(icmp->icmp_data, 0xa5, datalen); /* fill with pattern */
+    Gettimeofday((struct timeval *) icmp->icmp_data, NULL);
+
+    len = 8 + datalen;      /* checksum ICMP header and data */
+   // icmp->icmp_cksum = 0;
+   // icmp->icmp_cksum = in_cksum((u_short *) icmp, len);
+
+    Sendto(sockfd, sendbuf, len, 0, pr->sasend, pr->salen);
+}
+
+void sig_alrm(int signo)
+{
+    send_v4();
+
+    alarm(1);
+    return;
+}
 
 
 /*
   Retrieve the destination canonical IP address in presentation format. 
 */
+
 int retrieveDestinationCanonicalIpPresentationFormat(const char *server_vm, char *destination_canonical_ip_presentation_format)
 {
   struct hostent *hptr;
@@ -46,13 +162,6 @@ int retrieveDestinationCanonicalIpPresentationFormat(const char *server_vm, char
   }
 }
 
-struct tour_payload
-{
-    char IPaddress_list[1496];
-
-
-};
-
 /*
     Stores the IP addresses of the vm-tour in a comma-separated string to be passed as payload to the tour-members.
 */
@@ -89,21 +198,6 @@ struct ip * createIPPacket()
 
 }
 
-char * retrieveNextTourIpAddress( char * IPaddress_list, int last_visited_index )
-{
-    int i;
-    char * p;
-    p = strtok( IPaddress_list,"|" );
-
-    for(i=0;i<last_visited_index;i++)
-    {
-        p = strtok( NULL,"|" );         
-    }    
-    printf("Next IP address to be sent to : %s", p);
-
-    return p;
-}
-
 
 /*
     
@@ -130,13 +224,21 @@ int joinMulticastGroup( int sock, char * multicast_ip_address, char * joining_lo
     return 1;
 }
 
+
+
+
+
+
 int main(int argc, char const *argv[])
 {
     int         packet_socket, rt_sock, pg_sock, iptour_return;
     const int   on = 1;
-    char IPaddress_list[MAX_PAYLOAD_SIZE], IPmulticast_address[INET_ADDRSTRLEN] = "239.108.175.37";
+    char IPaddress_list[MAX_PAYLOAD_SIZE], IPmulticast_address[INET_ADDRSTRLEN] = "239.108.175.37", host[INET_ADDRSTRLEN];
     int port_number = 17537;
-
+    int             c;
+    struct addrinfo *ai;
+    char *h;
+    int datalen = 56;
     if( createIPTourString(IPaddress_list, argv, IPmulticast_address, port_number) == -1 )
     {
         printf("Error : Same node should not appear consequentively in the tour list..Exiting..\n");
@@ -175,6 +277,44 @@ int main(int argc, char const *argv[])
     }       
 
     printf("<time>   received source routing packet from <hostname>.\n");
+
+
+
+    //pinging**************************
+    
+
+    host = "192.168.1.101";
+
+    pid = IDENTIFIER & 0xffff;  /* ICMP ID field is 16 bits */
+    Signal(SIGALRM, sig_alrm);
+
+    ai = Host_serv(host, NULL, 0, 0);
+
+    h = Sock_ntop_host(ai->ai_addr, ai->ai_addrlen);
+    printf("PING %s (%s): %d data bytes\n",
+            ai->ai_canonname ? ai->ai_canonname : h,
+            h, datalen);
+
+        /* 4initialize according to protocol */
+    if (ai->ai_family == AF_INET) 
+    {
+        pr = &proto_v4;
+
+    } 
+    else
+        err_quit("unknown address family %d", ai->ai_family);
+
+    pr->sasend = ai->ai_addr;
+    pr->sarecv = Calloc(1, ai->ai_addrlen);
+    pr->salen = ai->ai_addrlen;
+    pr->icmpproto = IPPROTO_ICMP;
+    readloop();
+
+
+
+
+
+    //pinging**************************
 
 }
 
